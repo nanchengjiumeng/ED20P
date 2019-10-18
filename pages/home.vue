65af9002224c9731ace3779dedf60bd4570c3ae2
wang yu biao <1443937536@qq.com>
2019-10-18 10:25:15 +0800

    init

---
+++ pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>{{new Date()}}</div>
+    <div>+1</div>
+    <div>+5</div>
+    <div>+10</div>
+    <div>+20</div>
+    <div>book name</div>
+    <div>something wirted</div>
+  </div>
+</template>
+
+<script type='text/ecmascript-6'>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang='scss' scoped='' type='text/css'>
+
+</style>
